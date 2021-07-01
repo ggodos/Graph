5aecdaa66809bf2d242c13bdd86ae0215a0a5e9c
Alexander <37180898+MrSago@users.noreply.github.com>
2021-07-01 22:46:53 +0300

    upd2

--- start.bat
+++ start.bat
@@ -1,5 +1,18 @@
 
 @echo off
+
 pip install -r requirements.txt
+if not %errorlevel% == 0 (
+    echo Error while install requirements
+    echo Code: %errorlevel%
+    pause
+    exit
+)
+
 python Main.py
+if not %errorlevel% == 0 (
+    echo Error while run program
+    echo Code: %errorlevel%
+    pause
+)
 
